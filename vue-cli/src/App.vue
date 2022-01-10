eff82a4e3bf8999b4d24e8b42664f2bfe44e71c9
RHU5 <gusdnr814@naver.com>
2022-01-10 18:37:11 +0900

    # vue-cli로 프로젝트 생성 및 구조 파악, 배운 것 적용

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ str }}
+    <AppHeader v-on:renew="renewStr" v-bind:propsdata="str" />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function () {
+    return {
+      str: 'Test',
+    };
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = 'hi';
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+  <!-- Html -->
+</template>
+
+<script>
+// javascript
+// Vue instance options
+export default {};
+</script>
+
+<style>
+/* CSS */
+</style>
