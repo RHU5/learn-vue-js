389d4b3b127a276aaf194973b7c8210c6b07f50c
RHU5 <gusdnr814@naver.com>
2022-01-11 10:37:14 +0900

    # 배운 지식으로 Form 구성해보기

---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">ID: </label>
+        <input id="username" type="text" v-model="username" />
+      </div>
+      <div>
+        <label for="password">PW: </label>
+        <input id="password" type="text" v-model="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm: function () {
+      console.log(this.username, this.password);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const body = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, body)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
